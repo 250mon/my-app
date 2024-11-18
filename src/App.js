64a12d1640915e9f3e5f43940886d46d994f689a
Jye Young Song <lambki@naver.com>
2024-11-19 08:33:37 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Header = (props) => {
   return (
-    <div className="App">
-    Hello React!
+    <header>
+      <h1><a href="/">{props.title}</a></h1>
+    </header>
+  );
+};
+
+const Nav = (props) => {
+  const lis = props.topics.map((topic) => (
+    <li key={topic.id}><a href={"/content/" + topic.id}>{topic.title}</a></li>
+  ));
+
+  return (
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  );
+}
+
+const Article = (props) => {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
+function App() {
+  const topics = [
+    {id: 1, title: 'HTML', body: 'HTML is ...'},
+    {id: 2, title: 'CSS', body: 'CSS is ...'},
+    {id: 3, title: 'JavaScript', body: 'JavaScript is ...'}
+  ]
+  return (
+    <div>
+      <Header title="WEB" />
+      <Nav topics={topics} />
+      <Article title="Welcome" body="Hello, WEB"/>
+      <Article title="Hi" body="Hello, React"/>
     </div>
   );
 }
